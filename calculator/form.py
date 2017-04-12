6aad8ea49ad8a991fb62b73ab5f02e6180c57368
Vito <vitohuanqui>
2017-04-12 18:00:34 -0500

    duplicate code

--- calculator/form.py
+++ calculator/form.py
@@ -5,8 +5,3 @@ class NameForm(forms.Form):
 
     one = forms.IntegerField(label='first number')
     second = forms.IntegerField(label='seocnd number')
-
-
-class NameForm1(forms.Form):
-    one = forms.IntegerField(label='first number')
-    second = forms.IntegerField(label='seocnd number')
