af4cf2f2e1812fd07dd4233bf164dd7b98884376
Vito <vitohuanqui>
2017-04-12 17:57:02 -0500

    duplicate code

--- calculator/form.py
+++ calculator/form.py
@@ -1,6 +1,12 @@
 from django import forms
 
+
 class NameForm(forms.Form):
 
+    one = forms.IntegerField(label='first number')
+    second = forms.IntegerField(label='seocnd number')
+
+
+class NameForm1(forms.Form):
     one = forms.IntegerField(label='first number')
     second = forms.IntegerField(label='seocnd number')
