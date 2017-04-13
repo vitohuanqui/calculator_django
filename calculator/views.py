228f6eb9c163696ed07af61a457ac7f5c1c03092
Vito <vitohuanqui>
2017-04-12 19:17:14 -0500

    asd

--- calculator/views.py
+++ calculator/views.py
@@ -46,36 +46,3 @@ class Divide(APIView):
         except Exception as e:
             return Response({'result': 'there was an error ' + str(e)})
 
-class Multiply(APIView):
-
-    renderer_classes = (JSONPRenderer,)
-
-    @staticmethod
-    def get(request):
-        form = NameForm()
-
-        return render(request, 'name.html', {'form': form})
-
-    @staticmethod
-    def post(request):
-        form = NameForm(request.POST)
-        if form.is_valid():
-            a = form.cleaned_data['one']
-            b = form.cleaned_data['second']
-            data = multiply(a, b)
-            return render(request, 'name.html', {'data': data})
-
-
-class Divide(APIView):
-
-    renderer_classes = (JSONPRenderer,)
-
-    @staticmethod
-    def get(request):
-        try:
-            first_number = int(request.GET.get('a'))
-            second_number = int(request.GET.get('b'))
-            return Response({'result': first_number / second_number})
-        except Exception as e:
-            return Response({'result': 'there was an error ' + str(e)})
-
